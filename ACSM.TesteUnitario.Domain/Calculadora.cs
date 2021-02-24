19062a55f88f26b97343a6298a28ade367aa50d6
Ageu Machado <ageu.analista@gmail.com>
2021-02-23 22:14:59 -0300

    Adicionar arquivos de projeto.

---
+++ ACSM.TesteUnitario.Domain/Calculadora.cs
@@ -0,0 +1,9 @@
+﻿namespace ACSM.TesteUnitario.Domain
+{
+    public class Calculadora
+    {
+        public double Somar(double valor1, double valor2) => valor1 + valor2;
+
+        public double Dividir(int valor1, int valor2) => valor1 / valor2;
+    }
+}
---
+++ ACSM.TesteUnitario.Domain/StringsTools.cs
@@ -0,0 +1,10 @@
+﻿namespace ACSM.TesteUnitario.Domain
+{
+    public class StringsTools
+    {
+        public string Unir(string nome, string sobrenome)
+        {
+            return $"{nome} {sobrenome}";
+        }
+    }
+}
